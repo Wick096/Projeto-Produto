35a613ff62d93bcc483bf569427e7e2da8864309
Bruno <62807696+Wick096@users.noreply.github.com>
2022-02-23 12:55:59 -0300

    1°Commit

---
+++ src/servidor.js
@@ -0,0 +1,30 @@
+const porta = 3003
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const bancoDados = require('./bancoDados')
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+
+app.get('/produtos',(req,res,next)=>{
+    res.send(bancoDados.getProdutos())
+
+})
+
+app.get('/produtos/:id',(req,res,next) =>{
+    res.send(bancoDados.getProduto(req.params.id))
+})
+
+app.post('/produtos',(req,res,next)=>{
+    const produto = bancoDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)//JSON
+})
+app.listen(porta,()=>{
+    console.log(`Servidor executando na porta${porta}.`)
+
+})
